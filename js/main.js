3f23816859765e3900a4c702875ef7837c8571f6
Eman <emmanuelgatwech@gmail.com>
2019-04-02 11:28:01 +0300

    added sentence fragments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /**
  * main js file
- * handles stuff like animations, pop ups etc
  */
 const generateButton = document.querySelector("#generate");
 const customizeButton = document.querySelector("#customize");
@@ -9,63 +8,36 @@ const info = document.querySelector(".info");
 const quoteType = document.querySelector("#quoteType");
 
 /**
- * have this dynamically set
+ * Main heading
  */
-quoteType.innerText = "Buddha Quotes";
+quoteType.innerText = "Motivational Quotes";
 
-/**
- * DOM manipulation methods
- */
-const hide = element => {
-  element.style.display = "none";
-};
-
-/**
- * helper methods
- */
-const getLength = iter => {
-  if(iter.length === 0 || iter.length === undefined) {
-    return null;
-  } else {
-    return iter.length;
-  }
-};
-
-const getRandom = (iter) => {
-  return iter[Math.floor(Math.random() * Math.floor(getLength(iter)))];
-};
-
-const limitElements = (limit = 1) => {
-  return limit;
-}
 
 /**
  * element creators
  */
-const createQuote = () => {
-  let text = generateQuote();
-  const quote = document.createElement("article");
+const createQuotes = () => {
+  let newQuote = new Quote();
+  let generatedQuote = newQuote.generate();
+  const quote = document.createElement("li");
   const quoteText = document.createElement("blockquote");
-  const hr = document.createElement("hr");
-  quoteText.innerText = text;
-  quote.appendChild(quoteText);
-  quote.appendChild(hr);
-  hr.className = "styled";
+  quoteText.innerText = generatedQuote;
   quote.className = "quote";
-  quotes.appendChild(quote);
-
-  /**
-  * attach ids and classes
-  */
+  append(quote, quoteText);
 
   /**
   * hide info
   */
   hide(info);
+  return quote;
 };
 
+
+const generateQuote = () => {
+  let quote = createQuotes();
+  append(quotes, quote);
+};
 /**
  * event listenrs
  */
-generateButton.addEventListener("click", createQuote);
-customizeButton.addEventListener("click", customizeQuotes);
+generateButton.addEventListener("click", generateQuote);
--- js/quotes.js
+++ js/quotes.js
@@ -1,75 +1,10 @@
-const motivationalQuotes = [
-  {
-    id: 1,
-    owner: "Ronnie Coleman.",
-    content: "Ain't nothing to it but to do it."
-  },
-  {
-    id: 2,
-    owner: "Swami Vivekananda",
-    content:
-      "Take up one idea.Make that one idea your life-- think of it, dream of it, live on that idea.Let the brain, muscles, nerves, every part of your body be full of that idea, and just leave every other idea alone.This is the way to success."
-  },
-  {
-    id: 3,
-    owner: "Dolly Parton",
-    content:
-      "The way I see it, if you want the rainbow, you gotta put up with the rain."
-  },
-  {
-    id: 4,
-    owner: "Nathaniel Hawthorne",
-    content:
-      "Happiness is a butterfly, which when pursued, is always beyond your grasp, but which, if you will sit down quietly, may alight upon you."
-  },
-  {
-    id: 5,
-    owner: "Mary Kay Ash",
-    content:
-      "Don't limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve."
-  },
-  {
-    id: 6,
-    owner: "Audrey Hepburn",
-    content:
-      "As you grow older, you will discover that you have two hands, one for helping yourself, the other for helping others."
-  },
-  {
-    id: 7,
-    owner: "John Quincy Adams",
-    content:
-      "If your actions inspire others to dream more, learn more, do more, and become more, you are a leader."
-  },
-  {
-    id: 8,
-    owner: "Lupe Fiasco",
-    content:
-      "Life ain’t made to come around twice, that’s why I gotta get it right."
-  },
-  {
-    id: 9,
-    owner: "Pablo Picasso",
-    content: "Everything you can imagine is real."
-  },
-  {
-    id: 10,
-    owner: "T.I",
-    content: "No way should reflection be mistaken for glorification"
-  }
-];
-
-/**
- * allows you to customize different types of quotes
- */
-const customizeQuotes = () => {
-  console.log("customizing these quotes!");
-};
-
 /**
  * generates new quotes
  */
-const generateQuote = () => {
-  console.log("customizing these quotes!");
-  let quote = getRandom(motivationalQuotes);
-  return quote.content;
+function Quote (iter) {
+  this.quotes = iter;
+};
+
+Quote.prototype.generate = function () {
+  return extractFragments(fragments);
 };
